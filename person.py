7b443501bd9fd12eee5587651e21bf603909e4c4
Kuzevanova <olya.kuzevanova@bk.ru>
2019-12-10 21:19:32 +0500

    init

--- person.py
+++ person.py
@@ -13,9 +13,9 @@ def byId(id):
     person = Person.query.get(id)
     return jsonify(Person.json()) if Person else ''
 
-@personApi.route('/person/name/<string:name>/age/<string:age>/school_id/<int:planet_id>/sport_id/<int:power_id>')
-def put(name, age, school_id, sport_id):
-    person = Person(name=name, age=age, school_id=school_id, sport_id=sport_id)
+@personApi.route('/person/name/<string:name>/age/<string:age>/school_id')
+def put(name, age, school_id):
+    person = Person(name=name, age=age, school_id=school_id)
     db.session.add(Person)
     db.session.commit()
     return jsonify(Person.json()) if Person else ''
